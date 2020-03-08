bcfdec235482c93ed8211a3b8ddd1bbe3989987c
ami.n <i.ami.nakagawa@globis.co.jp>
2020-03-08 12:42:30 +0900

    Add solution to the problem

--- 012-7kyu-alternate-capitalization.js
+++ 012-7kyu-alternate-capitalization.js
@@ -1,3 +1,11 @@
+// Description:
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below.Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF'].See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Solution1
 function capitalize(s) {
     var even = []
     var odd = []
@@ -16,3 +24,10 @@ function capitalize(s) {
     odd = odd.join('')
     return [even, odd]
 };
+
+// Solution2
+function capitalize(s) {
+    var even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase : l).join("")
+    var odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase : l).join("")
+    return [even, odd]
+}
